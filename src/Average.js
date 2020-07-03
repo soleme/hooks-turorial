c9aab4f104f2cac8c587d2f5ca349c55a997ebe5
younjong park <soleme@gmail.com>
2020-07-03 22:42:42 +0900

    훅스관련 공부

---
+++ src/Average.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const getAverage = numbers =>{
+    console.log('평균값 계산중...');
+
+}
+
+const Average = () => {
+    return(
+        <div>
+
+
+        </div>
+    );
+}
+
+export default Average;
--- src/Info.js
+++ src/Info.js
@@ -1,37 +1,38 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action){
+    return{
+        ...state,
+        [action.name]:action.value
+    };
+}
 
 const Info = () => {
-    const[name, setName]= useState('');
-    const[nickName, setNickname] = useState('');
+    const[state, dispatch] = useReducer(reducer, {
+        name : '',
+        nickName : ''
+    });
 
-    const onChangeName = e => {
-        setName(e.target.value);
-    }
+    const{name, nickname}=state;
 
-    const onChangeNicknname = e =>{
-        setNickname(e.target.value);
-    }
+    const onChange = e =>{
+        dispatch(e.target);
+    };
 
     return(
         <div>
             <div>
-                <input value={name} onChange={onChangeName}/>
-                <input value={nickName} onChange={onChangeNicknname}/>
-
-
-            </div>
-
-            <div>
-                <b>이름 : {name}</b>
+                <input name="name" value={name} onChange={onChange}/>
+                <input name="nickname" value={nickname} onChange={onChange}/>
             </div>
             <div>
-                <b>닉네임 : {nickName}</b>
+                <b>이름 : </b> {name}
+            </div>
+            <div>
+                <b>닉네임 : </b> {nickname}
             </div>
         </div>
-
-
     );
-
 };
 
 export default Info;
